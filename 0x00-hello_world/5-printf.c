4c7d51a6b0b60afc94cfc0d3f03d1696e5dc8a95
ante neh <amareanteneh12@gmail.com>
2022-03-11 02:20:43 -1200

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+  *main - Entry point
+  *My hello, printf task
+  *Return:return 1 after program execution
+  */
+int main (void)
+{
+printf("
+
+
+[A[A[C[C[C[C[C[C[C[A[C
+
+
+[A[[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cwith proper grammer, but the outcome is apiece of art,");[D[D[D\n);
+return (0);
+}
