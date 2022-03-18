8c25718e60bf1cfe4ba72860e87d3bc9c66c588d
ante-neh <amareanteneh12@gmail.com>
2022-03-18 06:56:46 -0400

    print diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,29 +1,33 @@
 #include "main.h"
-#include <stdio.h>
-/**
- * print_diagsums - prints buffer in hexa
- * @a: cheese
- * @size: buffer2
- *
- * Return: Nothing.
- */
-void print_diagsums(int *a, int size)
-{
-	int i, j, s = 0, t = 0;
 
-	for (i = 0; i < size; i++)
+/**
+ * print_diagonal -  checks for checks for a digit (0 through 9).
+ * @n: n -  Variable
+ *
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int x, y;
+
+	if (n > 0)
 	{
-		for (j = 0; j < size; j++)
+		for (x = 1; x <= n; x++)
 		{
-			if (i == j)
+			for (y = 1; y <= n; y++)
 			{
-				s = s + a[(size + 1) * i];
+				if (x == y)
+				{
+					_putchar(92);
+					break;
+				}
+				_putchar(' ');
 			}
-			if (i + j == size - 1)
-			{
-				t = t + a[(size - 1) * (i + 1)];
-			}
+			_putchar('\n');
 		}
 	}
-	printf("%i, %i\n", s, t);
+	else
+	{
+		_putchar('\n');
+	}
 }
